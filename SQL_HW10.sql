ba48d73fc2ce9cf94c59e8a62c0ba3fbf09f81df
Omer Demircioglu <omer.demircioglu@boun.edu.tr>
2021-08-29 13:39:42 +0300

    Add files via upload

--- SQL_HW10.sql
+++ SQL_HW10.sql
@@ -1,20 +1,15 @@
 --city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte 
 --görebileceğimiz LEFT JOIN sorgusunu yazınız.
-
 SELECT c.city,co.country FROM CITY C
 LEFT JOIN COUNTRY CO  ON C.country_id=co.country_id
-
 --customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki 
 --first_name ve last_name isimlerini
 --birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
-
 select c.first_name,c.last_name from customer c
 right join payment p on
 c.customer_id=p.payment_id
-
 --customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki 
 --first_name ve last_name isimlerini birlikte 
 --görebileceğimiz FULL JOIN sorgusunu yazınız.
-
 select r.rental_id, c.first_name,c.last_name from customer c 
 full join rental r on c.customer_id=r.customer_id
